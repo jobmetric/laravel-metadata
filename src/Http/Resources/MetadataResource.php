f4fc87780d2c74fc35742003f6ebf2feeea593f1
Majid Mohammadian <majeedmohammadian@gmail.com>
2023-05-03 19:10:17 +0430

    add metadata resource

---
+++ src/Http/Resources/MetadataResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace JobMetric\Metadata\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+/**
+ * @property mixed $key
+ * @property mixed $value
+ * @property mixed $is_json
+ */
+class MetadataResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     *
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request): array|Arrayable|JsonSerializable
+    {
+        return [
+            $this->key => ($this->is_json) ? json_decode($this->value, true) : $this->value
+        ];
+    }
+}
