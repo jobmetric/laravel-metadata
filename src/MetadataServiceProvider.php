15a4258dbea268e780d039d8baf57717ed3a9433
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-16 00:50:39 +0330

    change provider file for remove service

--- src/MetadataServiceProvider.php
+++ src/MetadataServiceProvider.php
@@ -3,7 +3,6 @@
 namespace JobMetric\Metadata;
 
 use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
-use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
 use JobMetric\PackageCore\PackageCore;
 use JobMetric\PackageCore\PackageCoreServiceProvider;
 
@@ -11,14 +10,12 @@ class MetadataServiceProvider extends PackageCoreServiceProvider
 {
     /**
      * @throws MigrationFolderNotFoundException
-     * @throws RegisterClassTypeNotFoundException
      */
     public function configuration(PackageCore $package): void
     {
         $package->name('laravel-metadata')
             ->hasConfig()
             ->hasTranslation()
-            ->hasMigration()
-            ->registerClass('Metadata', Metadata::class);
+            ->hasMigration();
     }
 }
