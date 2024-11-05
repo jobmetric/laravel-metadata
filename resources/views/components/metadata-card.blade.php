3f234e36541ab09bbd67019095663b41adba623d
majidmohammadian <majeedmohammadian@gmail.com>
2024-11-05 23:10:36 +0330

    added trans for language key and add textarea type to metadata-card component

--- resources/views/components/metadata-card.blade.php
+++ resources/views/components/metadata-card.blade.php
@@ -9,17 +9,22 @@
         @foreach($items as $metadata_key => $metadata_value)
             <div class="mb-10">
                 <label class="form-label d-flex justify-content-between align-items-center">
-                    <span>{{ $metadata_value['label'] }}</span>
-                    <div class="text-gray-600 fs-7 d-none d-md-block d-lg-none d-xl-block">{{ $metadata_value['info'] }}</div>
+                    <span>{{ trans($metadata_value['label']) }}</span>
+                    <div class="text-gray-600 fs-7 d-none d-md-block d-lg-none d-xl-block">{{ trans($metadata_value['info']) }}</div>
                 </label>
+
+                @if($metadata_value['type'] === 'textarea')
+                    <textarea name="metadata[{{ $metadata_key }}]" class="form-control" rows="3" placeholder="{{ trans($metadata_value['placeholder']) }}">{{ $values[$metadata_key] ?? '' }}</textarea>
+                @endif
                 @if($metadata_value['type'] === 'text')
-                    <input type="text" name="metadata[{{ $metadata_key }}]" class="form-control" placeholder="{{ $metadata_value['placeholder'] }}" value="{{ $values[$metadata_key] ?? '' }}">
+                    <input type="text" name="metadata[{{ $metadata_key }}]" class="form-control" placeholder="{{ trans($metadata_value['placeholder']) }}" value="{{ $values[$metadata_key] ?? '' }}">
                 @endif
                 @if($metadata_value['type'] === 'number')
-                    <input type="number" name="metadata[{{ $metadata_key }}]" class="form-control" placeholder="{{ $metadata_value['placeholder'] }}" value="{{ $values[$metadata_key] ?? '' }}">
+                    <input type="number" name="metadata[{{ $metadata_key }}]" class="form-control" placeholder="{{ trans($metadata_value['placeholder']) }}" value="{{ $values[$metadata_key] ?? '' }}">
                 @endif
+
                 @error('metadata.' . $metadata_key)
-                <div class="form-errors text-danger fs-7 mt-2">{{ $message }}</div>
+                    <div class="form-errors text-danger fs-7 mt-2">{{ $message }}</div>
                 @enderror
             </div>
         @endforeach
