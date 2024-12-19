7e543a521ab308339521d73830514b16fcb76b67
majidmohammadian <majeedmohammadian@gmail.com>
2024-12-19 23:46:35 +0330

    changed metadata card based on new algorithm

--- resources/views/components/metadata-card.blade.php
+++ resources/views/components/metadata-card.blade.php
@@ -12,31 +12,8 @@
                  * @var \JobMetric\Metadata\ServiceType\Metadata $meta
                  */
             @endphp
-            {!! $meta->customField->render(hasErrorTagForm: true) !!}
+            {!! $meta->customField->render($values[$meta->customField->params['uniqName']], hasErrorTagForm: true) !!}
         @endforeach
-
-        {{--@foreach($items as $metadata_key => $metadata_value)
-            <div class="mb-10">
-                <label class="form-label d-flex justify-content-between align-items-center">
-                    <span>{{ trans($metadata_value['label']) }}</span>
-                    <div class="text-gray-600 fs-7 d-none d-md-block d-lg-none d-xl-block">{{ trans($metadata_value['info']) }}</div>
-                </label>
-
-                @if($metadata_value['type'] === 'textarea')
-                    <textarea name="metadata[{{ $metadata_key }}]" class="form-control" rows="3" placeholder="{{ trans($metadata_value['placeholder']) }}">{{ $values[$metadata_key] ?? '' }}</textarea>
-                @endif
-                @if($metadata_value['type'] === 'text')
-                    <input type="text" name="metadata[{{ $metadata_key }}]" class="form-control" placeholder="{{ trans($metadata_value['placeholder']) }}" value="{{ $values[$metadata_key] ?? '' }}">
-                @endif
-                @if($metadata_value['type'] === 'number')
-                    <input type="number" name="metadata[{{ $metadata_key }}]" class="form-control" placeholder="{{ trans($metadata_value['placeholder']) }}" value="{{ $values[$metadata_key] ?? '' }}">
-                @endif
-
-                @error('metadata.' . $metadata_key)
-                    <div class="form-errors text-danger fs-7 mt-2">{{ $message }}</div>
-                @enderror
-            </div>
-        @endforeach--}}
     </div>
 </div>
 <!--end::Metadata-->
