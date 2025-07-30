1645fd4d324216a6f150ae8ef56ee07b562b84ba
majidmohammadian <majeedmohammadian@gmail.com>
2025-07-30 22:24:16 +0330

    added phpdoc to MetaContract

--- src/Contracts/MetaContract.php
+++ src/Contracts/MetaContract.php
@@ -2,12 +2,27 @@
 
 namespace JobMetric\Metadata\Contracts;
 
+/**
+ * Interface MetaContract
+ *
+ * Defines the contract for Eloquent models that support metadata management.
+ *
+ * Implementing this interface requires the model to specify which metadata keys
+ * are allowed to be used for storage and retrieval, providing a layer of
+ * validation and security.
+ *
+ * @package JobMetric\Metadata
+ */
 interface MetaContract
 {
     /**
-     * metadata allow fields.
+     * Returns an array of allowed metadata keys that can be stored or accessed
+     * on the implementing model.
      *
-     * @return array
+     * This method is used to whitelist valid metadata fields and prevent
+     * unauthorized or unintended keys from being used.
+     *
+     * @return string[] List of allowed metadata keys.
      */
     public function metadataAllowFields(): array;
 }
