75cf00cdf496343fc5e122bf7075a9fbbd764348
majidmohammadian <majeedmohammadian@gmail.com>
2025-07-30 22:37:07 +0330

    added article migration table and model

---
+++ tests/Stubs/Models/Article.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace JobMetric\Metadata\Tests\Stubs\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use JobMetric\Metadata\Contracts\MetaContract;
+use JobMetric\Metadata\HasMeta;
+
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $status
+ *
+ * @method static create(string[] $array)
+ */
+class Article extends Model implements MetaContract
+{
+    use HasMeta;
+
+    public $timestamps = false;
+    protected $fillable = [
+        'title',
+        'status'
+    ];
+    protected $casts = [
+        'title' => 'string',
+        'status' => 'string',
+    ];
+
+    public function metadataAllowFields(): array
+    {
+        return [
+            'description',
+            'info'
+        ];
+    }
+}
