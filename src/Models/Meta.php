4bb2de671cb2ea5af9d439f1808eb9d43f8066d3
majidmohammadian <majeedmohammadian@gmail.com>
2025-08-01 16:23:30 +0330

    added phpdoc in model Meta

--- src/Models/Meta.php
+++ src/Models/Meta.php
@@ -2,62 +2,84 @@
 
 namespace JobMetric\Metadata\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\Builder;
 
 /**
- * @property int id
- * @property int metaable
- * @property int metaable_id
- * @property string metaable_type
- * @property string key
- * @property mixed value
- * @property boolean is_json
+ * Class Meta
+ *
+ * Represents a key-value metadata entry associated with any Eloquent model
+ * via a polymorphic relation.
+ *
+ * This model is designed to store arbitrary metadata for models such as posts,
+ * products, users, etc., allowing extensibility without altering their main table.
+ * It supports complex data types by marking values as JSON-encoded.
+ *
+ * @package JobMetric\Metadata
+ *
+ * @property int $id The primary identifier of the metadata row.
+ * @property string $metaable_type The class name of the related model.
+ * @property int $metaable_id The ID of the related model instance.
+ * @property string $key The name of the metadata key.
+ * @property mixed $value The associated metadata value.
+ * @property boolean $is_json Whether the value is stored as JSON.
+ *
+ * @property-read Model|MorphTo $metaable The related Eloquent model.
+ *
+ * @method static Builder|Meta whereMetaableType(string $metaable_type)
+ * @method static Builder|Meta whereMetaableId(int $metaable_id)
+ * @method static Builder|Meta whereKey(string $key)
+ * @method static Builder|Meta whereValue(mixed $value)
+ * @method static Builder|Meta whereIsJson(bool $is_json)
  */
 class Meta extends Model
 {
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
-        'metaable_id',
         'metaable_type',
+        'metaable_id',
         'key',
         'value',
         'is_json'
     ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
     protected $casts = [
+        'metaable_type' => 'string',
+        'metaable_id' => 'integer',
         'key' => 'string',
         'is_json' => 'boolean'
     ];
 
-    public function getTable()
+    /**
+     * Override the table name using config.
+     *
+     * @return string
+     */
+    public function getTable(): string
     {
         return config('metadata.tables.meta', parent::getTable());
     }
 
     /**
-     * metaable relationship
+     * Get the parent metaable model (morph-to relation).
      *
      * @return MorphTo
      */
     public function metaable(): MorphTo
     {
         return $this->morphTo();
-    }
-
-    /**
-     * Scope a query to only include meta by key.
-     *
-     * @param Builder $query
-     * @param string $key
-     *
-     * @return Builder
-     */
-    public function scopeOfKey(Builder $query, string $key): Builder
-    {
-        return $query->where('key', $key);
     }
 }
